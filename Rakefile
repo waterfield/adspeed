e85137cff299b8445acbc48476ba8bef231aed86
Jason Whitehorn <jason.whitehorn@gmail.com>
2012-06-25 14:34:24 -0500

    initial commit

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'rake'
+require 'echoe'
+
+Echoe.new('uniquify', '0.1.0') do |p|
+  p.description     = "Adspeed API"
+  p.url             = "http://github.com/waterfield/adspeed"
+  p.author          = "Waterfield Technologies"
+  p.email           = "info@waterfieldtech.com"
+  p.ignore_pattern  = ["tmp/*", "script/*"]
+  p.development_dependencies = []
+end
+
+Dir["#{File.dirname(__FILE__)}/tasks/*.rake"].sort.each { |ext| load ext }
---
+++ lib/adpseed.rb
@@ -0,0 +1,38 @@
+require 'uri'
+require 'net/http'
+
+class Adspeed
+  
+  def self.configure(api_key, api_secret)
+    @@api_key = api_key
+    @@api_secret = api_secret
+    @@api_url = "http://api.adspeed.com/"
+  end
+  
+  def self.get_api(method, params)
+    params = params.merge!({:key => @@api_key, :method => method})    
+    query = params.sort.map { |key, value| @"#{URI.escape(key)}=#{URI.escape(value)}"}.join("&")
+    
+    url = URI.parse(@@api_url + "?#{query}&md5=" + Digest::MD5.hexdigest(@@api_secret + query))
+       
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    res.body
+  end
+  
+  def self.post_api(method, params)
+    params = params.merge!({:key => @@api_key, :method => method})
+    
+    query = params.sort.map { |key, value| @"#{URI.escape(key)}=#{URI.escape(value)}"}.join("&")
+    query += ("&md5=" + Digest::MD5.hexdigest(@@api_secret + query))
+    
+    url = URI.parse @@api_url
+         
+    res = Net::HTTP.post_form(url, query)
+    res.body
+  end
+  
+  
+end
